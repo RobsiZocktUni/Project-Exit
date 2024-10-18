3ef7ce447d0c3c4b70a78157f75a7c4bc7391426
JBeckSolutions <jonas.beck552@gmail.com>
2024-10-18 10:39:17 +0200

    Update InventoryManager.cs
    Added more comments to the Code.

--- Project Exit/Assets/Inventory/InventoryManager.cs
+++ Project Exit/Assets/Inventory/InventoryManager.cs
@@ -5,9 +5,9 @@ using UnityEngine.UI;
 
 public class InventoryManager : MonoBehaviour
 {
-    public static InventoryManager Instance;
-    private List<KeyItem> inventoryItems = new List<KeyItem>();
-    public Transform inventoryListContainer;
+    public static InventoryManager Instance;    // Singelton
+    private List<KeyItem> inventoryItems = new List<KeyItem>();    // List with all the items that are currently in the inventory.
+    public Transform inventoryListContainer;    //Transform of the Container in the ui that holds all the InventoryTiles of the items.
 
     void Start()
     {
@@ -36,12 +36,12 @@ public class InventoryManager : MonoBehaviour
 
     private void DrawItems() // Adds the InventoryTiles of the items in the Inventory List to the Ui
     {
-        foreach (Transform item in inventoryListContainer)
+        foreach (Transform item in inventoryListContainer)    // Delets all current InventoryTiles to remove all the ones not needed anymore
         {
             Destroy(item.gameObject);
         }
 
-        foreach (var item in inventoryItems)
+        foreach (var item in inventoryItems)    //Adds all the InventoryTiles needed to the ui
         {
             GameObject newInventoryTile = Instantiate(item.InventoryTile, inventoryListContainer);
             newInventoryTile.GetComponent<Button>().onClick.AddListener(item.UseItem);
