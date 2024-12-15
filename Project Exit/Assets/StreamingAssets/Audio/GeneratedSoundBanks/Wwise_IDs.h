c144ad50b8239d4920175db87064c6df50fab6c2
RobsiZocktUni <lennart.hartmann@stud.hs-kempten.de>
2024-12-15 23:11:49 +0100

    added menu theme

--- Project Exit/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Project Exit/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,11 +13,12 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID PLAY_MENU_THEME = 2181630209U;
         static const AkUniqueID PLAY_SFX_SCHUBLADE_AUF = 1215060680U;
         static const AkUniqueID PLAY_SFX_STREICHHOLZ = 3417554753U;
         static const AkUniqueID PLAY_SFX_STREICHHOLZSCHACHTEL_AUFHEBEN = 3591392569U;
-        static const AkUniqueID PLAY_TEST_02 = 2788420171U;
         static const AkUniqueID STEPS = 1718617278U;
+        static const AkUniqueID STOP_MENU_THEME = 810056023U;
     } // namespace EVENTS
 
     namespace GAME_PARAMETERS
